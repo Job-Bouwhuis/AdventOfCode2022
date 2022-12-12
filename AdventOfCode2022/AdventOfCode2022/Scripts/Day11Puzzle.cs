293ad1a05726f63eb61b1e5c8633745c37156e88
Job Bouwhuis <jobbouwhuis@hotmail.com>
2022-12-12 21:46:24 +0100

    started day 12

--- AdventOfCode2022/AdventOfCode2022/Scripts/Day11Puzzle.cs
+++ AdventOfCode2022/AdventOfCode2022/Scripts/Day11Puzzle.cs
@@ -47,7 +47,7 @@ namespace AdventOfCode2022.Scripts
             Setup();
             Console.Clear();
 
-            int lcd = monkeys.Select(m => m.divisableBy).Aggregate(1, (x, y) => x * y);
+            int lcd = monkeys.Select(m => m.testValue).Aggregate(1, (x, y) => x * y);
             foreach (int i in 10000)
             {
                 Console.WriteLine(i);
@@ -59,6 +59,7 @@ namespace AdventOfCode2022.Scripts
                     }
                 }
             }
+            
             ulong result = 0;
             monkeys = monkeys.OrderByDescending(x => x.inspectedItems).ToList();
             result = monkeys[0].inspectedItems * monkeys[1].inspectedItems;
@@ -71,19 +72,72 @@ namespace AdventOfCode2022.Scripts
         public int monkeyID;
         public List<long> inventory = new();
         public ulong inspectedItems = 0;
-
+        public int testValue;
+        
         string operation;
         int toMonkeyIfTrue;
         int toMonkeyIfFalse;
         string rawOperation;
-        public int divisableBy;
 
-        // 2713310158
-        // 2635191806
+        public Monkee(string unparsedData)
+        {
+            string[] lines = unparsedData.Split("\r\n");
+
+            string line = lines[0];
+            {
+                string[] data = line.Split(" ");
+                monkeyID = TypeWorker.CastPrimitive<int>(data[1].TrimEnd(':'));
+            }
+            line = lines[1];
+            {
+                string[] data = line.Split(" ")[4..];
+                foreach (string item in data)
+                    inventory.Add(TypeWorker.CastPrimitive<long>(item.TrimEnd(',')));
+            }
+            line = lines[2];
+            rawOperation = line[23..];
+            line = lines[3];
+            testValue = TypeWorker.CastPrimitive<int>(line[21..]);
+            line = lines[4];
+            toMonkeyIfTrue = TypeWorker.CastPrimitive<int>(line[^1]);
+            line = lines[5];
+            toMonkeyIfFalse = TypeWorker.CastPrimitive<int>(line[^1]);
+        }
+
+
+        public void ThrowPart1(List<Monkee> monkeys)
+        {
+            var data = EvaluateConditionPart1();
+            Console.WriteLine($"monkey: {monkeyID}, data: {data.result}");
+            inspectedItems++;
+
+            if (data.condition)
+                monkeys[toMonkeyIfTrue].inventory.Add(data.result);
+            else
+                monkeys[toMonkeyIfFalse].inventory.Add(data.result);
+            inventory.RemoveAt(0);
+        }
+        public (long result, bool condition) EvaluateConditionPart1()
+        {
+            string[] operationData = GetNewestOperation().Split(' ');
+            long value = 0;
+            switch (operationData[1])
+            {
+                case "*":
+                    value = TypeWorker.CastPrimitive<long>(operationData[0]) * TypeWorker.CastPrimitive<long>(operationData[2]);
+                    break;
+                case "+":
+                    value = TypeWorker.CastPrimitive<long>(operationData[0]) + TypeWorker.CastPrimitive<long>(operationData[2]);
+                    break;
+            }
+            value /= 3;
+            return (value, value % testValue == 0);
+        }
+
 
         public void ThrowPart2(List<Monkee> monkeys, int lcd)
         {
-            
+
             var data = EvaluateConditionPart2(lcd);
             inspectedItems++;
 
@@ -106,76 +160,18 @@ namespace AdventOfCode2022.Scripts
                     value = TypeWorker.CastPrimitive<long>(operationData[0]) + TypeWorker.CastPrimitive<long>(operationData[2]);
                     break;
             }
-            
+
             value %= lcd;
-            return (value, value % divisableBy == 0);
+            return (value, value % testValue == 0);
         }
 
+        
         string GetNewestOperation()
         {
             string[] operationData = rawOperation.Split(' ');
             string lastOperationItem = operationData[1] == "old" ? inventory[0].ToString() : operationData[1];
             operation = $"{inventory[0]} {operationData[0]} {lastOperationItem}";
             return operation;
-        }
-
-        public void ThrowPart1(List<Monkee> monkeys)
-        {
-            var data = EvaluateConditionPart1();
-            Console.WriteLine($"monkey: {monkeyID}, data: {data.result}");
-            inspectedItems++;
-
-            if (data.condition)
-                monkeys[toMonkeyIfTrue].inventory.Add(data.result);
-            else
-                monkeys[toMonkeyIfFalse].inventory.Add(data.result);
-            inventory.RemoveAt(0);
-        }
-
-
-
-        public (long result, bool condition) EvaluateConditionPart1()
-        {
-            string[] operationData = GetNewestOperation().Split(' ');
-            long value = 0;
-            switch (operationData[1])
-            {
-                case "*":
-                    value = TypeWorker.CastPrimitive<long>(operationData[0]) * TypeWorker.CastPrimitive<long>(operationData[2]);
-                    break;
-                case "+":
-                    value = TypeWorker.CastPrimitive<long>(operationData[0]) + TypeWorker.CastPrimitive<long>(operationData[2]);
-                    break;
-            }
-            value /= 3;
-            return (value, value % divisableBy == 0);
-        }
-
-
-
-        public Monkee(string unparsedData)
-        {
-            string[] lines = unparsedData.Split("\r\n");
-
-            string line = lines[0];
-            {
-                string[] data = line.Split(" ");
-                monkeyID = TypeWorker.CastPrimitive<int>(data[1].TrimEnd(':'));
-            }
-            line = lines[1];
-            {
-                string[] data = line.Split(" ")[4..];
-                foreach (string item in data)
-                    inventory.Add(TypeWorker.CastPrimitive<long>(item.TrimEnd(',')));
-            }
-            line = lines[2];
-            rawOperation = line[23..];
-            line = lines[3];
-            divisableBy = TypeWorker.CastPrimitive<int>(line[21..]);
-            line = lines[4];
-            toMonkeyIfTrue = TypeWorker.CastPrimitive<int>(line[^1]);
-            line = lines[5];
-            toMonkeyIfFalse = TypeWorker.CastPrimitive<int>(line[^1]);
         }
     }
 }
---
+++ AdventOfCode2022/AdventOfCode2022/Scripts/Day12Puzzle.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace AdventOfCode2022.Scripts
+{
+    [Puzzle(12, "Path Finding")]
+    internal class Day12Puzzle : IMyPuzzle
+    {
+        public char[,] map;
+        Point inputSize;
+        string[] input;
+        public void Setup()
+        {
+            input = FileManager.ReadAllLines("Inputs/Day12.txt").ToStringArray();
+            inputSize = new(input.Length, input[1].Length);
+
+            map = new char[input.Length, input[0].Length];
+            foreach (int i in input.Length)
+            {
+
+                foreach (int j in input[i].Length)
+                    map[i, j] = input[i][j];
+            }
+        }
+
+        Tile GetPositionOfDestination()
+        {
+            foreach (int i in inputSize.X)
+            {
+                foreach (int j in inputSize.Y)
+                {
+                    if (map[i, j] == 'E')
+                        return new Tile() { X = i, Y = j };
+                }
+            }
+            throw new Exception("Destination not found");
+        }
+
+        public object? SolveFirst()
+        {
+            var start = new Tile();
+            start.Y = 0;
+            start.X = 0;
+
+            var finish = GetPositionOfDestination();
+            start.SetDistance(finish);
+
+            List<Tile> activeTiles = new();
+            List<Tile> visitedTiles = new();
+
+
+            return null;
+        }
+
+        public object? SolveSecond()
+        {
+            return null;
+        }
+
+        private static List<Tile> GetWalkableTiles(List<string> map, Tile currentTile, Tile targetTile)
+        {
+            var Temp = new List<Tile>()
+            {
+                new Tile { X = currentTile.X, Y = currentTile.Y - 1, Parent = currentTile, Cost = currentTile.Cost + 1 },
+                new Tile { X = currentTile.X, Y = currentTile.Y + 1, Parent = currentTile, Cost = currentTile.Cost + 1},
+                new Tile { X = currentTile.X - 1, Y = currentTile.Y, Parent = currentTile, Cost = currentTile.Cost + 1 },
+                new Tile { X = currentTile.X + 1, Y = currentTile.Y, Parent = currentTile, Cost = currentTile.Cost + 1 },
+            };
+            List<Tile> possibleTiles = new();
+            foreach (Tile tile in Temp)
+            {
+
+            }
+
+            var maxX = map.First().Length - 1;
+            var maxY = map.Count - 1;
+
+            return possibleTiles
+                    .Where(tile => tile.X >= 0 && tile.X <= maxX)
+                    .Where(tile => tile.Y >= 0 && tile.Y <= maxY)
+                    .Where(tile => map[tile.Y][tile.X] == ' ' || map[tile.Y][tile.X] == 'B')
+                    .ToList();
+        }
+
+        class Tile
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Cost { get; set; }
+            public int Distance { get; set; }
+            public int CostDistance => Cost + Distance;
+            public Tile Parent { get; set; }
+
+            //The distance is essentially the estimated distance, ignoring walls to our target. 
+            //So how many tiles left and right, up and down, ignoring walls, to get there. 
+            public void SetDistance(Tile target)
+            {
+                Distance = Math.Abs(target.X - X) + Math.Abs(target.Y - Y);
+            }
+        }
+    }
+}

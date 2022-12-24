0b5f62b89208c7c50702ae5599397b15886f7baf
Job Bouwhuis <jobbouwhuis@hotmail.com>
2022-12-24 20:59:19 +0100

    changes

--- AdventOfCode2022/AdventOfCode2022/Scripts/Day12Puzzle.cs
+++ AdventOfCode2022/AdventOfCode2022/Scripts/Day12Puzzle.cs
@@ -7,31 +7,33 @@ namespace AdventOfCode2022.Scripts
     [Puzzle(12, "Path Finding")]
     internal class Day12Puzzle : IMyPuzzle
     {
-        public char[,] map;
-        Point inputSize;
+        const int STEP_COST = 1;
+        const int DESTINATION_HEIGHT_VALUE = 'E';
+        static Tile[,] map;
+        static Point inputSize;
         string[] input;
         public void Setup()
         {
             input = FileManager.ReadAllLines("Inputs/Day12.txt").ToStringArray();
-            inputSize = new(input.Length, input[1].Length);
+            inputSize = new(input[1].Length, input.Length);
 
-            map = new char[input.Length, input[0].Length];
-            foreach (int i in input.Length)
-            {
-
-                foreach (int j in input[i].Length)
-                    map[i, j] = input[i][j];
-            }
+            map = new Tile[input.Length, input[0].Length];
+            foreach (int y in input.Length)
+                foreach (int x in input[y].Length)
+                    if (input[y][x] == 'E')
+                        map[y, x] = new(x, y, DESTINATION_HEIGHT_VALUE);
+                    else
+                        map[y, x] = new(x, y, input[y][x]);
         }
 
-        Tile GetPositionOfDestination()
+        Tile GetDestinationTile()
         {
-            foreach (int i in inputSize.X)
+            foreach (int y in inputSize.Y)
             {
-                foreach (int j in inputSize.Y)
+                foreach (int x in inputSize.X)
                 {
-                    if (map[i, j] == 'E')
-                        return new Tile() { X = i, Y = j };
+                    if (map[y, x].h == DESTINATION_HEIGHT_VALUE)
+                        return map[y, x];
                 }
             }
             throw new Exception("Destination not found");
@@ -39,18 +41,49 @@ namespace AdventOfCode2022.Scripts
 
         public object? SolveFirst()
         {
-            var start = new Tile();
-            start.Y = 0;
-            start.X = 0;
-
-            var finish = GetPositionOfDestination();
-            start.SetDistance(finish);
-
-            List<Tile> activeTiles = new();
-            List<Tile> visitedTiles = new();
-
-
-            return null;
+            Tile goal = GetDestinationTile();
+            Stack<Tile> frontier = new();
+            frontier.Push(map[0, 0]);
+            List<Tile> explored = new();
+            Dictionary<Tile, Tile> cameFrom = new();
+            
+            while (frontier.Count > 0)
+            {
+                Tile current = frontier.Pop();
+                explored.Add(current);
+                if(current == goal)
+                {
+                    break;
+                }
+                foreach (Tile neighbor in current.GetNeighbors())
+                {
+                    if (explored.Contains(neighbor))
+                        continue;
+                    if (!frontier.Contains(neighbor))
+                    {
+                        neighbor.h = current.h + STEP_COST;
+                        cameFrom.Add(neighbor, current);
+                        frontier.Push(neighbor);
+                    }
+                    else
+                    {
+                        if (neighbor.h >= current.h + STEP_COST)
+                        {
+                            //neighbor.h = current.h + STEP_COST;
+                            cameFrom.Add(neighbor, current);
+                        }
+                    }
+                }
+            }
+            
+            Tile PathCurrent = goal;
+            List<Tile> path = new();
+            while (PathCurrent != map[0, 0])
+            {
+                path.Add(PathCurrent);
+                PathCurrent = cameFrom[PathCurrent];
+            }   
+            return path.Count;
         }
 
         public object? SolveSecond()
@@ -58,45 +91,51 @@ namespace AdventOfCode2022.Scripts
             return null;
         }
 
-        private static List<Tile> GetWalkableTiles(List<string> map, Tile currentTile, Tile targetTile)
-        {
-            var Temp = new List<Tile>()
+        public record Tile(int x, int y)
+        { 
+            public int h { get; set; }
+            public Tile[] GetNeighbors()
             {
-                new Tile { X = currentTile.X, Y = currentTile.Y - 1, Parent = currentTile, Cost = currentTile.Cost + 1 },
-                new Tile { X = currentTile.X, Y = currentTile.Y + 1, Parent = currentTile, Cost = currentTile.Cost + 1},
-                new Tile { X = currentTile.X - 1, Y = currentTile.Y, Parent = currentTile, Cost = currentTile.Cost + 1 },
-                new Tile { X = currentTile.X + 1, Y = currentTile.Y, Parent = currentTile, Cost = currentTile.Cost + 1 },
-            };
-            List<Tile> possibleTiles = new();
-            foreach (Tile tile in Temp)
-            {
-
+                int lengthX = inputSize.X;
+                int lengthY = inputSize.Y;
+                var result = new List<Tile>();
+                
+                //top left
+                if (x > 0 && y > 0)
+                    result.Add(map[y - 1, x - 1]);
+                
+                //top right
+                if (x + 1 < lengthX && y > 0)
+                    result.Add(map[y - 1, x + 1]);
+                
+                //bottom right
+                if (x + 1 < lengthX && y + 1 < lengthY)
+                    result.Add(map[y + 1, x + 1]);
+                
+                //bottom left
+                if (x > 0 && y + 1 < lengthY)
+                    result.Add(map[y + 1, x - 1]);
+                
+                //right
+                if (x + 1 < lengthX && y < lengthY)
+                    result.Add(map[y, x + 1]);
+                
+                //left
+                if (x > 0 && y < lengthY)
+                    result.Add(map[y, x - 1]);
+                
+                //top
+                if (y + 1 < lengthY && x < lengthX)
+                    result.Add(map[y + 1, x]);
+                
+                //bottom
+                if (y > 0 && x < lengthX)
+                    result.Add(map[y - 1, x]);
+                return result.ToArray();
             }
-
-            var maxX = map.First().Length - 1;
-            var maxY = map.Count - 1;
-
-            return possibleTiles
-                    .Where(tile => tile.X >= 0 && tile.X <= maxX)
-                    .Where(tile => tile.Y >= 0 && tile.Y <= maxY)
-                    .Where(tile => map[tile.Y][tile.X] == ' ' || map[tile.Y][tile.X] == 'B')
-                    .ToList();
-        }
-
-        class Tile
-        {
-            public int X { get; set; }
-            public int Y { get; set; }
-            public int Cost { get; set; }
-            public int Distance { get; set; }
-            public int CostDistance => Cost + Distance;
-            public Tile Parent { get; set; }
-
-            //The distance is essentially the estimated distance, ignoring walls to our target. 
-            //So how many tiles left and right, up and down, ignoring walls, to get there. 
-            public void SetDistance(Tile target)
+            public Tile(int x, int y, int h) : this(x, y)
             {
-                Distance = Math.Abs(target.X - X) + Math.Abs(target.Y - Y);
+                this.h = h;
             }
         }
     }
